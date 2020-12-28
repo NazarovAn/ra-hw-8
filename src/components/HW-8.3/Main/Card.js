0451eb376f73e928fc2986e20406ce049067783b
NazarovAn <naatonchrome@gmail.com>
2020-12-28 15:49:58 +0400

    Loading animaton added

--- src/components/HW-8.3/Main/Card.js
+++ src/components/HW-8.3/Main/Card.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Loading from '../Loading/Loading';
 
 export default function Card(props) {
+  const [isLoading, setLoading] = useState(true);
+
   return (
     <li className="neto__main_card">
-      <img src={ props.image } alt="..." />
+      { isLoading ? <Loading /> : null }
+      <img 
+        src={ props.image } 
+        alt="..."
+        onLoad={ () => setLoading(false) }
+        style={{ display: isLoading ? 'none' : null }}
+      />
       <span>{ props.title }</span>
       <p>{ props.content }</p>
     </li>
